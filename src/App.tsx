0fa192a437f0f3a47f443a2a14f6852040acd686
athi-v <athenkosivinqi25@gmail.com>
2024-05-11 18:42:41 +0200

    update: integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Box, Button } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import AboutUs from "./pages/aboutus/AboutUs";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <Box>
-      <Button variant="text" color="error">
-        Test
-      </Button>
-    </Box>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
