87cf15ddbe9d56678baad7ff5055039b2ab789b1
athi-v <athenkosivinqi25@gmail.com>
2024-05-15 17:51:54 +0200

    update: navigation

--- src/components/navbar/DashboardNavbar.tsx
+++ src/components/navbar/DashboardNavbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { ListItem, Stack } from "@mui/material";
+
+const dashboardItem = [
+  { id: "0", title: "Overview" },
+  { id: "1", title: "Statistics" },
+  { id: "3", title: "Covid Test" },
+];
 
 const DashboardNavbar = () => {
   return (
-    <div>DashboardNavbar</div>
-  )
-}
+    <Stack>
+      <Stack spacing={2}>
+        {dashboardItem.map((item) => (
+          <ListItem>{item.title}</ListItem>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
 
-export default DashboardNavbar
+export default DashboardNavbar;
