2a9c7a01e59366e10934a9a75c8792cd153f42fa
athi-v <athenkosivinqi25@gmail.com>
2024-05-31 17:44:03 +0200

    ui update

--- src/components/navbar/MobileBottomNavbar.tsx
+++ src/components/navbar/MobileBottomNavbar.tsx
@@ -1,46 +1,57 @@
-import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
-import { MdAnalytics } from "react-icons/md";
-import { RiDashboardHorizontalFill } from "react-icons/ri";
-import { useNavigate } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { MdAnalytics, MdLogout } from 'react-icons/md';
+import { RiDashboardHorizontalFill } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
 const dashboardItem = [
-  {
-    id: "0",
-    title: "Dashboard",
-    link: "/dashboard",
-    icon: <RiDashboardHorizontalFill />,
+    {
+        id: '0',
+        title: 'Dashboard',
+        link: '/dashboard',
+        icon: <RiDashboardHorizontalFill />,
+    },
+    {
+        id: '1',
+        title: 'Statistics',
+        link: '/statistics',
+        icon: <MdAnalytics />,
+    },
+    {
+      id: '1',
+      title: 'Logout',
+      link: '/',
+      icon: <MdLogout />,
   },
-  { id: "1", title: "Statistics", link: "/statistics", icon: <MdAnalytics /> },
 ];
 
 const MobileBottomNavbar = () => {
-  const navigate = useNavigate();
-  return (
-    <Paper
-      sx={{
-        display: { xs: "blcok", lg: "none" },
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-      elevation={3}
-    >
-      <BottomNavigation showLabels>
-        {dashboardItem.map((item) => (
-          <BottomNavigationAction
-          sx={{
-            fontWeight: '700'
-          }}
-            key={item.id}
-            label={item.title}
-            icon={item.icon}
-            onClick={() => navigate(item.link)}
-          />
-        ))}
-      </BottomNavigation>
-    </Paper>
-  );
+    const navigate = useNavigate();
+    return (
+        <Paper
+            sx={{
+                display: { xs: 'blcok', lg: 'none' },
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
+            }}
+            elevation={3}
+        >
+            <BottomNavigation showLabels>
+                {dashboardItem.map((item) => (
+                    <BottomNavigationAction
+                        sx={{
+                            fontWeight: '700',
+                        }}
+                        key={item.id}
+                        label={item.title}
+                        icon={item.icon}
+                        onClick={() => navigate(item.link)}
+                    />
+                ))}
+            </BottomNavigation>
+        </Paper>
+    );
 };
 
 export default MobileBottomNavbar;
